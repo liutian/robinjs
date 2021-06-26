cd409d78f6db34ee10a97a19af5f0fe72bda5079
shunshun.liu <shunshun.liu@weimob.com>
2021-06-26 20:09:06 +0800

    feat: ts编译器通过远程加载

--- packages/bootstrap/src/webworker.ts
+++ packages/bootstrap/src/webworker.ts
@@ -1,12 +1,31 @@
 import { createCode } from '@robinjs/core';
 import { InputMessage, OutputMessage } from './types';
 
+const defaultOptions: optionsType = {
+  tsWorkerUrl:
+    'https://typescript.azureedge.net/cdn/4.3.4/monaco/min/vs/language/typescript/tsWorker.js',
+};
+
+const options: optionsType = Object.assign(
+  {},
+  defaultOptions,
+  JSON.parse((self as any).name)
+);
+
+// fix tsWorker
+(self as any).define = function () {
+  // console.log('')
+};
+(self as any).importScripts(options.tsWorkerUrl);
+
+const transpileModule = (self as any).ts.transpileModule;
+
 (self as any).postMessage('ok');
 
 self.addEventListener('message', (event) => {
   const { type, metadata, path } = event.data as InputMessage;
   if (type === 'transformCode') {
-    const code = createCode(metadata);
+    const code = createCode(metadata, { transpileModule });
     const message: OutputMessage = {
       type: 'emitCode',
       path,
@@ -16,3 +35,7 @@ self.addEventListener('message', (event) => {
     (self as any).postMessage(message);
   }
 });
+
+type optionsType = {
+  tsWorkerUrl: string;
+};
